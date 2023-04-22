aeab91a74e89a51794bc709bac50768965cf3150
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-22 16:42:36 +0530

    [ADDED]:[Nirmla] SankandLadder Setup With Welcome Message

---
+++ src/com/snackeladder/basic/Snakeandladder.java
@@ -0,0 +1,16 @@
+package com.snackeladder.basic;
+
+public class Snakeandladder {
+
+	public static void main(String[] args) {
+		int position = 0;
+		
+	System.out.println("Player 1 Start position is = "+position);
+  
+  
+  
+	}
+
+}
+
+
