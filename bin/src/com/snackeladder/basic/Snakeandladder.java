e1e2adc6727bec1ebb6530aace60bf5cadc6efd3
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-23 16:06:03 +0530

    [MODIFIED]:[Nirmla] check Player Winning Position in SankandLadder

--- bin/src/com/snackeladder/basic/Snakeandladder.java
+++ bin/src/com/snackeladder/basic/Snakeandladder.java
@@ -1,24 +1,53 @@
 package com.snackeladder.basic;
 
 import java.util.Random;
-
 public class Snakeandladder {
-
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		/*
 		 * Variable defining player position in game
 		 */
 		int position = 0;
 		
-	System.out.println("Player 1 Start position is = "+position);
+		final int NO_PLAY = 1;
+		final int LADDER = 2;
+		final int SNAKE = 3;
   /*
    *  Random Class Object to generate Die Number between 1-6
    */
     Random random = new Random();
-     int dieno = random.nextInt(6)+1;
-  System.out.println(" Die Number:"+dieno);
-  position = position + dieno;
-  System.out.println("New Position After Rolling Die:"+position);
+	System.out.println("Player 1 Start position is = "+position);
+	
+	while(position != 100) {
+	
+    int dieno = random.nextInt(6)+1;
+   System.out.println("Die Number="+dieno);
+   
+  int option =  random.nextInt(3)+1;
+ System.out.println("The Player Option Number= "+option);
+   switch(option){
+  case NO_PLAY:
+	 System.out.println("Option = NO_PLAy");
+	  position = position;
+	  break;
+  case LADDER:
+	  System.out.println("OPtion = LADDER"); 
+	  position = position + dieno;
+	 if(position > 100) {
+		  position = position - dieno;
+	  }
+	  break;
+  case SNAKE:
+	  System.out.println("Option = Snake"); 
+	  position = position - dieno;
+	  if(position < 0) {
+		  position = 0;
+   }
+	  break;
+   }
+	  System.out.println("New position= "+position);
+	  System.out.println("Winning Position= "+position);
+	  
+		  }
 	}
 
 }
